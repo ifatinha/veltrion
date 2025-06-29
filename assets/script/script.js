837c04adf3a7ee85fde8e5bf73a881699f61b1bf
Fatima Ferreira <ifatinha99@gmail.com>
2025-06-29 07:12:17 -0300

    header project

---
+++ assets/script/script.js
@@ -0,0 +1,20 @@
+import { openModal, closeModal } from "./modules/menuModalController.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const functionsToCall = [
+    { name: "openModal", func: openModal },
+    { name: "closeModal", func: closeModal },
+  ];
+
+  functionsToCall.forEach(({ name, func }) => {
+    if (typeof func === "function") {
+      try {
+        func();
+      } catch (error) {
+        console.log(`Erro ao executar ${name}: `, error);
+      }
+    } else {
+      console.error(`${name} não é uma função válida.`);
+    }
+  });
+});
